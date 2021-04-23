74d47270c102bf17556ecdbb2fec3b5f3dbbbd3b
Brant Hajek <branthajek@gmail.com>
2021-04-23 16:22:37 -0700

    split up code into feature modules

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AuthComponent } from './auth.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+const authRoutes: Routes = [
+  { path: 'auth', component: AuthComponent}
+];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild(authRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+const shoppingListRoutes: Routes = [
+  { path: 'shopping-list', component: ShoppingListComponent},
+];
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingListEditComponent
+  ],
+  imports: [
+    SharedModule,
+    FormsModule,
+    RouterModule.forChild(shoppingListRoutes)
+  ]
+})
+export class ShoppingListModule { }
